da86d064afb4f8df524c9bfb050e442d97d557d8
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2020-12-15 08:57:12 +0800

    lab11 Backup 05

--- lab11/CPU_CDE_AXI/mycpu_axi_verify/rtl/myCPU/mycpu_top.v
+++ lab11/CPU_CDE_AXI/mycpu_axi_verify/rtl/myCPU/mycpu_top.v
@@ -1,8 +1,8 @@
 `include "mycpu.h"
 module mycpu_top(
-    input         int,
-    input         aclk,
-    input         aresetn,
+    input         int         ,
+    input         aclk        ,
+    input         aresetn     ,
     // ar
     output [ 3:0] arid        ,
     output [31:0] araddr      ,
@@ -51,16 +51,96 @@ module mycpu_top(
     output [31:0] debug_wb_rf_wdata
 );
 
+wire        cpu_inst_req    ;
+wire        cpu_inst_wr     ;
+wire [ 1:0] cpu_inst_size   ;
+wire [31:0] cpu_inst_addr   ;
+wire [ 3:0] cpu_inst_wstrb  ;
+wire [31:0] cpu_inst_wdata  ;
+wire        cpu_inst_addr_ok;
+wire        cpu_inst_data_ok;
+wire [31:0] cpu_inst_rdata  ;
+
+wire        cpu_data_req    ;
+wire        cpu_data_wr     ;
+wire [ 1:0] cpu_data_size   ;
+wire [31:0] cpu_data_addr   ;
+wire [ 3:0] cpu_data_wstrb  ;
+wire [31:0] cpu_data_wdata  ;
+wire        cpu_data_addr_ok;
+wire        cpu_data_data_ok;
+wire [31:0] cpu_data_rdata  ;
+
 cpu_axi_interface u_axi_interface(
-    .clk              (aclk             ),
-    .resetn           (aresetn),
-    .arid             (arid)
+    .clk         (aclk            ),
+    .resetn      (aresetn         ),
+    // inst sram
+    .inst_req    (cpu_inst_req    ),
+    .inst_wr     (cpu_inst_wr     ),
+    .inst_size   (cpu_inst_size   ),
+    .inst_addr   (cpu_inst_addr   ),
+    .inst_wstrb  (cpu_inst_wstrb  ),
+    .inst_wdata  (cpu_inst_wdata  ),
+    .inst_addr_ok(cpu_inst_addr_ok),
+    .inst_data_ok(cpu_inst_data_ok),
+    .inst_rdata  (cpu_inst_rdata  ),
+    // data sram
+    .data_req    (cpu_data_req    ),
+    .data_wr     (cpu_data_wr     ),
+    .data_size   (cpu_data_size   ),
+    .data_addr   (cpu_data_addr   ),
+    .data_wstrb  (cpu_data_wstrb  ),
+    .data_wdata  (cpu_data_wdata  ),
+    .data_addr_ok(cpu_data_addr_ok),
+    .data_data_ok(cpu_data_data_ok),
+    .data_rdata  (cpu_data_rdata  ),
+    //ar
+    .arid        (arid            ),
+    .araddr      (araddr          ),
+    .arlen       (arlen           ),
+    .arsize      (arsize          ),
+    .arburst     (arburst         ),
+    .arlock      (arlock          ),
+    .arcache     (arcache         ),
+    .arprot      (arprot          ),
+    .arvalid     (arvalid         ),
+    .arready     (arready         ),
+    //r
+    .rid         (rid             ),
+    .rdata       (rdata           ),
+    .rresp       (rresp           ),
+    .rlast       (rlast           ),
+    .rvalid      (rvalid          ),
+    .rready      (rready          ),
+    //aw
+    .awid        (awid            ),
+    .awaddr      (awaddr          ),
+    .awlen       (awlen           ),
+    .awsize      (awsize          ),
+    .awburst     (awburst         ),
+    .awlock      (awlock          ),
+    .awcache     (awcache         ),
+    .awprot      (awprot          ),
+    .awvalid     (awvalid         ),
+    .awready     (awready         ),
+    //w
+    .wid         (wid             ),
+    .wdata       (wdata           ),
+    .wstrb       (wstrb           ),
+    .wlast       (wlast           ),
+    .wvalid      (wvalid          ),
+    .wready      (wready          ),
+    //b
+    .bid         (bid             ),
+    .bresp       (bresp           ),
+    .bvalid      (bvalid          ),
+    .bready      (bready          )
 );
 
 mycpu_core u_cpu_core(
-    .clk              (cpu_clk   ),
-    .resetn           (cpu_resetn),  //low active
-
+    .clk              (cpu_clk         ),
+    .resetn           (cpu_resetn      ),  //low active
+    // inst sram
     .inst_sram_req    (cpu_inst_req    ),
     .inst_sram_wr     (cpu_inst_wr     ),
     .inst_sram_size   (cpu_inst_size   ),
@@ -70,7 +150,7 @@ mycpu_core u_cpu_core(
     .inst_sram_addr_ok(cpu_inst_addr_ok),
     .inst_sram_data_ok(cpu_inst_data_ok),
     .inst_sram_rdata  (cpu_inst_rdata  ),
-
+    // data sram
     .data_sram_req    (cpu_data_req    ),
     .data_sram_wr     (cpu_data_wr     ),
     .data_sram_size   (cpu_data_size   ),
@@ -80,7 +160,6 @@ mycpu_core u_cpu_core(
     .data_sram_addr_ok(cpu_data_addr_ok),
     .data_sram_data_ok(cpu_data_data_ok),
     .data_sram_rdata  (cpu_data_rdata  ),
-
     //debug interface
     .debug_wb_pc      (debug_wb_pc      ),
     .debug_wb_rf_wen  (debug_wb_rf_wen  ),
@@ -90,179 +169,3 @@ mycpu_core u_cpu_core(
 
 endmodule
 
-module mycpu_core(
-    input         clk,
-    input         resetn,
-    // inst sram interface
-    output        inst_sram_req,
-    output        inst_sram_wr,
-    output [ 1:0] inst_sram_size,
-    output [31:0] inst_sram_addr,
-    output [ 3:0] inst_sram_wstrb,
-    output [31:0] inst_sram_wdata,
-    input         inst_sram_addr_ok,
-    input         inst_sram_data_ok,
-    input  [31:0] inst_sram_rdata,
-    // data sram interface
-    output        data_sram_req,
-    output        data_sram_wr,
-    output [ 1:0] data_sram_size,
-    output [31:0] data_sram_addr,
-    output [ 3:0] data_sram_wstrb,
-    output [31:0] data_sram_wdata,
-    input         data_sram_addr_ok,
-    input         data_sram_data_ok,
-    input  [31:0] data_sram_rdata,
-    // trace debug interface
-    output [31:0] debug_wb_pc,
-    output [ 3:0] debug_wb_rf_wen,
-    output [ 4:0] debug_wb_rf_wnum,
-    output [31:0] debug_wb_rf_wdata
-);
-reg         reset;
-always @(posedge clk) reset <= ~resetn;
-
-wire         ds_allowin;
-wire         es_allowin;
-wire         ms_allowin;
-wire         ws_allowin;
-wire         fs_to_ds_valid;
-wire         ds_to_es_valid;
-wire         es_to_ms_valid;
-wire         ms_to_ws_valid;
-wire [`FS_TO_DS_BUS_WD -1:0] fs_to_ds_bus;
-wire [`DS_TO_ES_BUS_WD -1:0] ds_to_es_bus;
-wire [`ES_TO_MS_BUS_WD -1:0] es_to_ms_bus;
-wire [`MS_TO_WS_BUS_WD -1:0] ms_to_ws_bus;
-wire [`WS_TO_RF_BUS_WD -1:0] ws_to_rf_bus;
-wire [`BR_BUS_WD       -1:0] br_bus;
-wire [`ES_TO_DS_BUS_WD -1:0] es_to_ds_bus;
-wire [`MS_TO_DS_BUS_WD -1:0] ms_to_ds_bus;
-
-
-// exc processing: flush & epc
-wire [31:0] ws_pc_gen_exc;
-wire        exc_flush;
-wire        ms_ex;
-
-
-// IF stage
-if_stage if_stage(
-    .clk             (clk               ),
-    .reset           (reset             ),
-    //allowin
-    .ds_allowin      (ds_allowin        ),
-    //brbus
-    .br_bus          (br_bus            ),
-    //outputs
-    .fs_to_ds_valid  (fs_to_ds_vali     ),
-    .fs_to_ds_bus    (fs_to_ds_bus      ),
-    //flush
-    .ws_pc_gen_exc   (ws_pc_gen_exc     ),
-    .exc_flush       (exc_flush         ),
-    // inst sram interface
-    .inst_sram_req    (inst_sram_req    ),
-    .inst_sram_wr     (inst_sram_wr     ),
-    .inst_sram_size   (inst_sram_size   ),
-    .inst_sram_addr   (inst_sram_addr   ),
-    .inst_sram_wstrb  (inst_sram_wstrb  ),
-    .inst_sram_wdata  (inst_sram_wdata  ),
-    .inst_sram_addr_ok(inst_sram_addr_ok),
-    .inst_sram_data_ok(inst_sram_data_ok),
-    .inst_sram_rdata  (inst_sram_rdata  )
-);
-// ID stage
-id_stage id_stage(
-    .clk            (clk            ),
-    .reset          (reset          ),
-    //allowin
-    .es_allowin     (es_allowin     ),
-    .ds_allowin     (ds_allowin     ),
-    //from fs
-    .fs_to_ds_valid (fs_to_ds_valid ),
-    .fs_to_ds_bus   (fs_to_ds_bus   ),
-    //from es raw
-    .es_to_ds_bus   (es_to_ds_bus   ),
-    //from ms raw
-    .ms_to_ds_bus   (ms_to_ds_bus   ),
-    //to es
-    .ds_to_es_valid (ds_to_es_valid ),
-    .ds_to_es_bus   (ds_to_es_bus   ),
-    //to fs
-    .br_bus         (br_bus         ),
-    //flush
-    .exc_flush      (exc_flush      ),
-    //to rf: for write back
-    .ws_to_rf_bus   (ws_to_rf_bus   )
-);
-// EXE stage
-exe_stage exe_stage(
-    .clk              (clk              ),
-    .reset            (reset            ),
-    //allowin
-    .ms_allowin       (ms_allowin       ),
-    .es_allowin       (es_allowin       ),
-    //from ds
-    .ds_to_es_valid   (ds_to_es_valid   ),
-    .ds_to_es_bus     (ds_to_es_bus     ),
-    //to ms
-    .es_to_ms_valid   (es_to_ms_valid   ),
-    .es_to_ms_bus     (es_to_ms_bus     ),
-    //to ds raw
-    .es_to_ds_bus     (es_to_ds_bus     ),
-    //flush
-    .exc_flush        (exc_flush        ),
-    .ms_ex            (ms_ex            ),
-    // data sram interface
-    .data_sram_req    (data_sram_req    ),
-    .data_sram_wr     (data_sram_wr     ),
-    .data_sram_size   (data_sram_size   ),
-    .data_sram_addr   (data_sram_addr   ),
-    .data_sram_wstrb  (data_sram_wstrb  ),
-    .data_sram_wdata  (data_sram_wdata  ),
-    .data_sram_addr_ok(data_sram_addr_ok)
-);
-// MEM stage
-mem_stage mem_stage(
-    .clk              (clk              ),
-    .reset            (reset            ),
-    //allowin
-    .ws_allowin       (ws_allowin       ),
-    .ms_allowin       (ms_allowin       ),
-    //from es
-    .es_to_ms_valid   (es_to_ms_valid   ),
-    .es_to_ms_bus     (es_to_ms_bus     ),
-    //to ws
-    .ms_to_ws_valid   (ms_to_ws_valid   ),
-    .ms_to_ws_bus     (ms_to_ws_bus     ),
-    //to ds raw
-    .ms_to_ds_bus     (ms_to_ds_bus     ),
-    //flush
-    .exc_flush        (exc_flush        ),
-    .ms_ex            (ms_ex            ),
-    //from data-sram
-    .data_sram_data_ok(data_sram_data_ok),
-    .data_sram_rdata  (data_sram_rdata  )
-);
-// WB stage
-wb_stage wb_stage(
-    .clk              (clk              ),
-    .reset            (reset            ),
-    //allowin
-    .ws_allowin       (ws_allowin       ),
-    //from ms
-    .ms_to_ws_valid   (ms_to_ws_valid   ),
-    .ms_to_ws_bus     (ms_to_ws_bus     ),
-    //to rf: for write back
-    .ws_to_rf_bus     (ws_to_rf_bus     ),
-    //flush
-    .ws_pc_gen_exc    (ws_pc_gen_exc    ),
-    .exc_flush        (exc_flush        ),
-    //trace debug interface
-    .debug_wb_pc      (debug_wb_pc      ),
-    .debug_wb_rf_wen  (debug_wb_rf_wen  ),
-    .debug_wb_rf_wnum (debug_wb_rf_wnum ),
-    .debug_wb_rf_wdata(debug_wb_rf_wdata)
-);
-
-endmodule
