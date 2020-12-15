e2947bc8ae168d4690d7b44662aa5485b8487a70
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2020-12-15 08:33:55 +0800

    lab11 Backup 04

--- lab11/CPU_CDE_AXI/mycpu_axi_verify/rtl/myCPU/mycpu_top.v
+++ lab11/CPU_CDE_AXI/mycpu_axi_verify/rtl/myCPU/mycpu_top.v
@@ -1,4 +1,95 @@
 `include "mycpu.h"
+module mycpu_top(
+    input         int,
+    input         aclk,
+    input         aresetn,
+    // ar
+    output [ 3:0] arid        ,
+    output [31:0] araddr      ,
+    output [ 7:0] arlen       ,
+    output [ 2:0] arsize      ,
+    output [ 1:0] arburst     ,
+    output [ 1:0] arlock      ,
+    output [ 3:0] arcache     ,
+    output [ 2:0] arprot      ,
+    output        arvalid     ,
+    input         arready     ,
+    //  r
+    input  [ 3:0] rid         ,
+    input  [31:0] rdata       ,
+    input  [ 1:0] rresp       , // ignore
+    input         rlast       , // ignore
+    input         rvalid      ,
+    output        rready      ,
+    // aw
+    output [ 3:0] awid        ,
+    output [31:0] awaddr      ,
+    output [ 7:0] awlen       ,
+    output [ 2:0] awsize      ,
+    output [ 1:0] awburst     ,
+    output [ 1:0] awlock      ,
+    output [ 1:0] awcache     ,
+    output [ 2:0] awprot      ,
+    output        awvalid     ,
+    input         awready     ,
+    //  w
+    output [ 3:0] wid         ,
+    output [31:0] wdata       ,
+    output [ 3:0] wstrb       ,
+    output        wlast       ,
+    output        wvalid      ,
+    input         wready      ,
+    //  b
+    input  [ 3:0] bid         , // ignore
+    input  [ 1:0] bresp       , // ignore
+    input         bvalid      ,
+    output        bready      ,
+    // debug
+    output [31:0] debug_wb_pc,
+    output [ 3:0] debug_wb_rf_wen,
+    output [ 4:0] debug_wb_rf_wnum,
+    output [31:0] debug_wb_rf_wdata
+);
+
+cpu_axi_interface u_axi_interface(
+    .clk              (aclk             ),
+    .resetn           (aresetn),
+    .arid             (arid)
+);
+
+mycpu_core u_cpu_core(
+    .clk              (cpu_clk   ),
+    .resetn           (cpu_resetn),  //low active
+
+    .inst_sram_req    (cpu_inst_req    ),
+    .inst_sram_wr     (cpu_inst_wr     ),
+    .inst_sram_size   (cpu_inst_size   ),
+    .inst_sram_wstrb  (cpu_inst_wstrb  ),
+    .inst_sram_addr   (cpu_inst_addr   ),
+    .inst_sram_wdata  (cpu_inst_wdata  ),
+    .inst_sram_addr_ok(cpu_inst_addr_ok),
+    .inst_sram_data_ok(cpu_inst_data_ok),
+    .inst_sram_rdata  (cpu_inst_rdata  ),
+
+    .data_sram_req    (cpu_data_req    ),
+    .data_sram_wr     (cpu_data_wr     ),
+    .data_sram_size   (cpu_data_size   ),
+    .data_sram_wstrb  (cpu_data_wstrb  ),
+    .data_sram_addr   (cpu_data_addr   ),
+    .data_sram_wdata  (cpu_data_wdata  ),
+    .data_sram_addr_ok(cpu_data_addr_ok),
+    .data_sram_data_ok(cpu_data_data_ok),
+    .data_sram_rdata  (cpu_data_rdata  ),
+
+    //debug interface
+    .debug_wb_pc      (debug_wb_pc      ),
+    .debug_wb_rf_wen  (debug_wb_rf_wen  ),
+    .debug_wb_rf_wnum (debug_wb_rf_wnum ),
+    .debug_wb_rf_wdata(debug_wb_rf_wdata)
+);
+
+endmodule
+
 module mycpu_core(
     input         clk,
     input         resetn,
