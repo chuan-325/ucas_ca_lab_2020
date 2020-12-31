b96ba54397142b67e17ddc94d4e2249988342dd1
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2020-12-31 22:03:26 +0800

    lab16: modules place in different files

--- lab16/cache_verify/rtl/cache/cache.v
+++ lab16/cache_verify/rtl/cache/cache.v
@@ -510,25 +510,3 @@ data_ram_bank data_ram_bank3_1(
 
 endmodule
 
-//////////////////////////////////////////
-module dirty_ram(
-    input         clk,
-    input         resetn,
-    //r
-    input  [ 7:0] raddr,
-    output        rdata,
-    //w
-    input         we,       //1:enable
-    input  [ 7:0] waddr,
-    input         wdata);
-
-reg [255:0] rf;
-always @(posedge clk) begin
-    if (!resetn)
-        rf <= 0;
-    else if (we)
-        rf[waddr]<= wdata;
-end
-
-assign rdata = rf[raddr];
-endmodule
