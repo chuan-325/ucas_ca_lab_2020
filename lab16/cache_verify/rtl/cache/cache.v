291e92277fdc13634c0c4bd0771bb296824820b7
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2020-12-31 22:01:50 +0800

    lab16: simulation FAIL: cacheres wrong at index 00

--- lab16/cache_verify/rtl/cache/cache.v
+++ lab16/cache_verify/rtl/cache/cache.v
@@ -28,6 +28,95 @@ module cache(
     output [127:0] wr_data  ,
     input          wr_rdy   );
 
+/**********DECLARATION**********/
+reg  [ 22:0]  pseudo_random_23;
+
+//Tag Compare
+wire         way0_hit;
+wire         way1_hit;
+wire         cache_hit;
+
+//Data Select
+//select data from two ways
+wire [127:0]  way0_data;
+wire [127:0]  way1_data;
+wire [31:0]   way0_load_word;
+wire [31:0]   way1_load_word;
+wire [31:0]   load_res;
+reg           replace_way;
+wire [127:0]  replace_data;
+
+//Request Buffer
+//save information from input ports
+reg           op_r;
+reg   [ 7:0]  index_r;
+reg   [19:0]  tag_r;
+reg   [ 3:0]  offset_r;
+reg   [ 3:0]  wstrb_r;
+reg   [31:0]  wdata_r;
+reg           busy;
+reg   [127:0] replace_data_r;
+
+wire         way0_v;
+wire         way1_v;
+wire [19:0]  way0_tag;
+wire [19:0]  way1_tag;
+
+//dirty_ram_0
+wire [7:0]   dirty_ram_0_raddr;
+wire         dirty_ram_0_rd;
+wire         dirty_ram_0_we;
+wire [7:0]   dirty_ram_0_waddr;
+wire         dirty_ram_0_wd;
+//dirty_ram_1
+wire [7:0]   dirty_ram_1_raddr;
+wire         dirty_ram_1_rd;
+wire         dirty_ram_1_we;
+wire [7:0]   dirty_ram_1_waddr;
+wire         dirty_ram_1_wd;
+
+//BANK
+//data_ram_bank0_0
+wire [3:0] data_ram_bank0_0_we;
+wire [7:0] data_ram_bank0_0_addr;
+wire [31:0]data_ram_bank0_0_wdata;
+wire [31:0]data_ram_bank0_0_rdata;
+//data_ram_bank1_0
+wire [3:0] data_ram_bank1_0_we;
+wire [7:0] data_ram_bank1_0_addr;
+wire [31:0]data_ram_bank1_0_wdata;
+wire [31:0]data_ram_bank1_0_rdata;
+//data_ram_bank2_0
+wire [3:0] data_ram_bank2_0_we;
+wire [7:0] data_ram_bank2_0_addr;
+wire [31:0]data_ram_bank2_0_wdata;
+wire [31:0]data_ram_bank2_0_rdata;
+//data_ram_bank3_0
+wire [3:0] data_ram_bank3_0_we;
+wire [7:0] data_ram_bank3_0_addr;
+wire [31:0]data_ram_bank3_0_wdata;
+wire [31:0]data_ram_bank3_0_rdata;
+//data_ram_bank0_1
+wire [3:0] data_ram_bank0_1_we;
+wire [7:0] data_ram_bank0_1_addr;
+wire [31:0]data_ram_bank0_1_wdata;
+wire [31:0]data_ram_bank0_1_rdata;
+//data_ram_bank1_1
+wire [3:0] data_ram_bank1_1_we;
+wire [7:0] data_ram_bank1_1_addr;
+wire [31:0]data_ram_bank1_1_wdata;
+wire [31:0]data_ram_bank1_1_rdata;
+//data_ram_bank2_1
+wire [3:0] data_ram_bank2_1_we;
+wire [7:0] data_ram_bank2_1_addr;
+wire [31:0]data_ram_bank2_1_wdata;
+wire [31:0]data_ram_bank2_1_rdata;
+//data_ram_bank3_1
+wire [3:0] data_ram_bank3_1_we;
+wire [7:0] data_ram_bank3_1_addr;
+wire [31:0]data_ram_bank3_1_wdata;
+wire [31:0]data_ram_bank3_1_rdata;
+
 //main state machine
 reg [2:0] cstate;
 reg [2:0] nstate;
@@ -37,8 +126,11 @@ parameter MISS    = 3'd2;
 parameter REPLACE = 3'd3;
 parameter REFILL  = 3'd4;
 
+
+/**********LOGIC**********/
+
 always@(posedge clk) begin
-    if(~resetn) begin
+    if (~resetn) begin
         cstate <= IDLE;
     end
     else begin
@@ -49,35 +141,35 @@ end
 always@(*) begin
     case(cstate)
         IDLE: begin
-            if(valid)
+            if (valid)
                 nstate = LOOKUP;
             else
                 nstate = cstate;
         end
 
         LOOKUP:begin
-            if(cache_hit)
+            if (cache_hit)
                 nstate = IDLE;
             else
                 nstate = MISS;
         end
 
         MISS:begin
-            if(wr_rdy)
+            if (wr_rdy)
                 nstate = REPLACE;
             else
                 nstate = cstate;
         end
 
         REPLACE:begin
-            if(rd_rdy)
+            if (rd_rdy)
                 nstate = REFILL;
             else
                 nstate = cstate;
         end
 
         REFILL:begin
-            if(ret_last)
+            if (ret_last)
                 nstate = IDLE;
             else
                 nstate = REFILL;
@@ -93,33 +185,33 @@ reg start;
 reg [31:0] rdata_r;
 reg [1:0] rd_cnt;
 always @(posedge clk) begin
-    if(!resetn) begina
+    if (!resetn) begin
         rd_cnt <= 2'b00;
     end
-    else if(ret_valid) begin
+    else if (ret_valid) begin
         rd_cnt <= rd_cnt + 2'b01;
     end
 end
 always@(posedge clk) begin
-    if(!resetn)
+    if (!resetn)
         rdata_r <= 0;
-    else if(cstate == LOOKUP & cache_hit)
+    else if (cstate == LOOKUP & cache_hit)
         rdata_r <= load_res;
-    else if(offset_r[3:2] == 2'b00 & rd_cnt == 2'b00 & ret_valid)
+    else if (offset_r[3:2] == 2'b00 & rd_cnt == 2'b00 & ret_valid)
         rdata_r <= ret_data;
-    else if(offset_r[3:2] == 2'b01 & rd_cnt == 2'b01 & ret_valid)
+    else if (offset_r[3:2] == 2'b01 & rd_cnt == 2'b01 & ret_valid)
         rdata_r <= ret_data;
-    else if(offset_r[3:2] == 2'b10 & rd_cnt == 2'b10 & ret_valid)
+    else if (offset_r[3:2] == 2'b10 & rd_cnt == 2'b10 & ret_valid)
         rdata_r <= ret_data;
-    else if(offset_r[3:2] == 2'b11 & rd_cnt == 2'b11 & ret_valid)
+    else if (offset_r[3:2] == 2'b11 & rd_cnt == 2'b11 & ret_valid)
         rdata_r <= ret_data;
 end
 always@(posedge clk) begin
-    if(!resetn)
+    if (!resetn)
         start <= 0;
-    else if(valid)
+    else if (valid)
         start <= 1;
-    else if(data_ok)
+    else if (data_ok)
         start <= 0;
 end
 assign addr_ok = (cstate == LOOKUP);
@@ -136,32 +228,32 @@ wire [19:0]    replace_addr;
 reg  [19:0]    replace_addr_r;
 assign replace_addr = replace_way? way1_tag : way0_tag;
 always@(posedge clk) begin
-    if(!resetn)
+    if (!resetn)
         wr_req_r <= 0;
-    else if(cstate == LOOKUP & nstate == MISS & ((dirty_ram_1_rd == 1)&replace_way | (dirty_ram_0_rd == 1)&~replace_way))
+    else if (cstate == LOOKUP & nstate == MISS & ((dirty_ram_1_rd == 1)&replace_way | (dirty_ram_0_rd == 1)&~replace_way))
         wr_req_r <= 1;
-    else if(wr_rdy)
+    else if (wr_rdy)
         wr_req_r <= 0;
 end
 always@(posedge clk)begin
-    if(!resetn) begin
+    if (!resetn) begin
         replace_addr_r <= 0;
     end
-    if(cstate == LOOKUP) begin
+    if (cstate == LOOKUP) begin
         replace_addr_r <= replace_addr;
     end
-    else if(cstate == REFILL) begin
+    else if (cstate == REFILL) begin
         replace_addr_r <= 0;
     end
 end
 always@(posedge clk)begin
-    if(!resetn) begin
+    if (!resetn) begin
         replace_data_r <= 0;
     end
-    if(cstate == LOOKUP) begin
+    if (cstate == LOOKUP) begin
         replace_data_r <= replace_data;
     end
-    else if(cstate == REFILL) begin
+    else if (cstate == REFILL) begin
         replace_data_r <= 0;
     end
 end
@@ -173,16 +265,8 @@ assign wr_data = replace_data_r;
 
 //Request Buffer
 //save information from input ports
-reg           op_r;
-reg   [ 7:0]  index_r;
-reg   [19:0]  tag_r;
-reg   [ 3:0]  offset_r;
-reg   [ 3:0]  wstrb_r;
-reg   [31:0]  wdata_r;
-reg           busy;
-reg   [127:0] replace_data_r;
 always@(posedge clk)begin
-    if(!resetn || (busy & data_ok)) begin
+    if (!resetn || (busy & data_ok)) begin
         op_r <= 0;
         index_r <= 0;
         tag_r <= 0;
@@ -191,7 +275,7 @@ always@(posedge clk)begin
         wdata_r <= 0;
         busy <= 0;
     end
-    else if(cstate == IDLE & valid) begin
+    else if (cstate == IDLE & valid) begin
         op_r <= op;
         index_r <= index;
         tag_r <= tag;
@@ -203,9 +287,7 @@ always@(posedge clk)begin
 end
 
 //Tag Compare
-wire         way0_hit;
-wire         way1_hit;
-wire         cache_hit;
+
 assign way0_hit = way0_v && (way0_tag == tag_r);
 assign way1_hit = way1_v && (way1_tag == tag_r);
 //? assign way0_hit = way0_v && (way0_tag == tag_r & cstate != IDLE);
@@ -214,13 +296,6 @@ assign cache_hit = way0_hit || way1_hit;
 
 //Data Select
 //select data from two ways
-wire [127:0]  way0_data;
-wire [127:0]  way1_data;
-wire [31:0]   way0_load_word;
-wire [31:0]   way1_load_word;
-wire [31:0]   load_res;
-reg           replace_way;
-wire [127:0]  replace_data;
 assign way0_data = {data_ram_bank3_0_rdata,data_ram_bank2_0_rdata,data_ram_bank1_0_rdata,data_ram_bank0_0_rdata};
 assign way1_data = {data_ram_bank3_1_rdata,data_ram_bank2_1_rdata,data_ram_bank1_1_rdata,data_ram_bank0_1_rdata};
 assign way0_load_word = (offset_r[3:2] == 2'd0) ? data_ram_bank0_0_rdata:
@@ -238,14 +313,13 @@ always@(posedge clk) begin
     if (!resetn) begin
         replace_way <= 0;
     end
-    else if (catste == REFILL && ret_last) begin
+    else if (cstate == REFILL && ret_last) begin
         replace_way <= pseudo_random_23[0];
     end
 end
 
 //LFSR
 //generate psudo-random numbers
-reg  [ 22:0]  pseudo_random_23;
 always @ (posedge clk) begin
     if (!resetn) begin
         pseudo_random_23 <= 23'b100_1010_0101_0010_1000_1010;
@@ -264,7 +338,7 @@ wire [23:0]tag_v_ram_0_rdata;
 assign tag_v_ram_0_we = (cstate == REFILL & replace_way == 0)? 3'b111:0;
 assign tag_v_ram_0_addr = busy? index_r : valid? index :0;
 assign tag_v_ram_0_wdata = {tag_r,4'b0001};
-/*name*/(
+tag_v_ram tag_v_ram_0(
 .clka(clk)                 ,
 .wea(tag_v_ram_0_we)       ,
 .addra(tag_v_ram_0_addr)   ,
@@ -279,17 +353,13 @@ wire [23:0]tag_v_ram_1_rdata;
 assign tag_v_ram_1_we = (cstate == REFILL & replace_way == 1)? 3'b111:0;
 assign tag_v_ram_1_addr = busy? index_r : valid? index :0;
 assign tag_v_ram_1_wdata = {tag_r,4'b0001};
-/*name*/(
+tag_v_ram tag_v_ram_1(
 .clka(clk)                 ,
 .wea(tag_v_ram_1_we)       ,
 .addra(tag_v_ram_1_addr)   ,
 .dina(tag_v_ram_1_wdata)   ,
 .douta(tag_v_ram_1_rdata) );
 
-wire         way0_v;
-wire         way1_v;
-wire [19:0]  way0_tag;
-wire [19:0]  way1_tag;
 assign way0_v   = tag_v_ram_0_rdata[0];
 assign way1_v   = tag_v_ram_1_rdata[0];
 assign way0_tag = tag_v_ram_0_rdata[23:4];
@@ -297,16 +367,11 @@ assign way1_tag = tag_v_ram_1_rdata[23:4];
 
 //DIRTY
 //dirty_ram_0
-wire [7:0]   dirty_ram_0_raddr;
-wire         dirty_ram_0_rd;
-wire         dirty_ram_0_we;
-wire [7:0]   dirty_ram_0_waddr;
-wire         dirty_ram_0_wd;
 assign dirty_ram_0_raddr = busy?index_r: valid? index: 0;
 assign dirty_ram_0_waddr = busy?index_r: valid? index: 0;
 assign dirty_ram_0_wd = (op_r == 1);
 assign dirty_ram_0_we = (cstate == LOOKUP & way0_hit & op_r == 1) | (cstate == REFILL & replace_way == 0 & op_r == 1);
-/*name*/(
+dirty_ram dirty_ram_0(
 .clk(clk),
 .resetn(resetn),
 .raddr(dirty_ram_0_raddr),
@@ -315,16 +380,11 @@ assign dirty_ram_0_we = (cstate == LOOKUP & way0_hit & op_r == 1) | (cstate == R
 .wdata(dirty_ram_0_wd));
 
 //dirty_ram_1
-wire [7:0]   dirty_ram_1_raddr;
-wire         dirty_ram_1_rd;
-wire         dirty_ram_1_we;
-wire [7:0]   dirty_ram_1_waddr;
-wire         dirty_ram_1_wd;
 assign dirty_ram_1_raddr = busy?index_r: valid? index: 0;
 assign dirty_ram_1_waddr = busy?index_r: valid? index: 0;
 assign dirty_ram_1_wd = (op_r == 1);
 assign dirty_ram_1_we = (cstate == LOOKUP & way0_hit & op_r == 1) | (cstate == REFILL & replace_way == 0 & op_r == 1);
-/*name*/(
+dirty_ram dirty_ram_1(
 .clk(clk),
 .resetn(resetn),
 .raddr(dirty_ram_1_raddr),
@@ -334,10 +394,6 @@ assign dirty_ram_1_we = (cstate == LOOKUP & way0_hit & op_r == 1) | (cstate == R
 
 //BANK
 //data_ram_bank0_0
-wire [3:0] data_ram_bank0_0_we;
-wire [7:0] data_ram_bank0_0_addr;
-wire [31:0]data_ram_bank0_0_wdata;
-wire [31:0]data_ram_bank0_0_rdata;
 assign data_ram_bank0_0_we =  (cstate == LOOKUP & cache_hit & way0_hit &offset_r[3:2] == 2'b00 & op_r == 1) ? wstrb_r://hit store
                               (cstate == REFILL & rd_cnt == 2'b00 & ret_valid & replace_way == 0) ? 4'b1111://refill
                               0;
@@ -354,7 +410,7 @@ assign data_ram_bank0_0_wdata = (cstate == LOOKUP & cache_hit & offset_r[3:2] ==
                                                                               wdata_r:
                                                     ret_data:
                                 0;
-/*name*/(
+data_ram_bank data_ram_bank0_0(
 .clka(clk),
 .wea(data_ram_bank0_0_we),
 .addra(data_ram_bank0_0_addr),
@@ -362,14 +418,10 @@ assign data_ram_bank0_0_wdata = (cstate == LOOKUP & cache_hit & offset_r[3:2] ==
 .douta(data_ram_bank0_0_rdata));
 
 //data_ram_bank1_0
-wire [3:0] data_ram_bank1_0_we;
-wire [7:0] data_ram_bank1_0_addr;
-wire [31:0]data_ram_bank1_0_wdata;
-wire [31:0]data_ram_bank1_0_rdata;
 assign data_ram_bank1_0_we =  data_ram_bank0_0_we;
 assign data_ram_bank1_0_addr = data_ram_bank0_0_addr;
 assign data_ram_bank1_0_wdata = data_ram_bank0_0_wdata;
-/*name*/(
+data_ram_bank data_ram_bank1_0(
 .clka(clk),
 .wea(data_ram_bank1_0_we),
 .addra(data_ram_bank1_0_addr),
@@ -377,14 +429,10 @@ assign data_ram_bank1_0_wdata = data_ram_bank0_0_wdata;
 .douta(data_ram_bank1_0_rdata));
 
 //data_ram_bank2_0
-wire [3:0] data_ram_bank2_0_we;
-wire [7:0] data_ram_bank2_0_addr;
-wire [31:0]data_ram_bank2_0_wdata;
-wire [31:0]data_ram_bank2_0_rdata;
 assign data_ram_bank2_0_we =  data_ram_bank0_0_we;
 assign data_ram_bank2_0_addr = data_ram_bank0_0_addr;
 assign data_ram_bank2_0_wdata = data_ram_bank0_0_wdata;
-/*name*/(
+data_ram_bank data_ram_bank2_0(
 .clka(clk),
 .wea(data_ram_bank2_0_we),
 .addra(data_ram_bank2_0_addr),
@@ -392,14 +440,10 @@ assign data_ram_bank2_0_wdata = data_ram_bank0_0_wdata;
 .douta(data_ram_bank2_0_rdata));
 
 //data_ram_bank3_0
-wire [3:0] data_ram_bank3_0_we;
-wire [7:0] data_ram_bank3_0_addr;
-wire [31:0]data_ram_bank3_0_wdata;
-wire [31:0]data_ram_bank3_0_rdata;
 assign data_ram_bank3_0_we =  data_ram_bank0_0_we;
 assign data_ram_bank3_0_addr = data_ram_bank0_0_addr;
 assign data_ram_bank3_0_wdata = data_ram_bank0_0_wdata;
-/*name*/(
+data_ram_bank data_ram_bank3_0(
 .clka(clk),
 .wea(data_ram_bank3_0_we),
 .addra(data_ram_bank3_0_addr),
@@ -407,10 +451,6 @@ assign data_ram_bank3_0_wdata = data_ram_bank0_0_wdata;
 .douta(data_ram_bank3_0_rdata));
 
 //data_ram_bank0_1
-wire [3:0] data_ram_bank0_1_we;
-wire [7:0] data_ram_bank0_1_addr;
-wire [31:0]data_ram_bank0_1_wdata;
-wire [31:0]data_ram_bank0_1_rdata;
 assign data_ram_bank0_1_we =  (cstate == LOOKUP & cache_hit & way1_hit &offset_r[3:2] == 2'b00 & op_r == 1) ? wstrb_r://hit store
                               (cstate == REFILL & rd_cnt == 2'b00 & ret_valid & replace_way == 0) ? 4'b1111://refill
                               0;
@@ -427,7 +467,7 @@ assign data_ram_bank0_1_wdata = (cstate == LOOKUP & cache_hit & offset_r[3:2] ==
                                                                               wdata_r:
                                                     ret_data:
                                 0;
-/*name*/(
+data_ram_bank data_ram_bank0_1(
 .clka(clk),
 .wea(data_ram_bank0_1_we),
 .addra(data_ram_bank0_1_addr),
@@ -435,14 +475,10 @@ assign data_ram_bank0_1_wdata = (cstate == LOOKUP & cache_hit & offset_r[3:2] ==
 .douta(data_ram_bank0_1_rdata));
 
 //data_ram_bank1_1
-wire [3:0] data_ram_bank1_1_we;
-wire [7:0] data_ram_bank1_1_addr;
-wire [31:0]data_ram_bank1_1_wdata;
-wire [31:0]data_ram_bank1_1_rdata;
 assign data_ram_bank1_1_we =  data_ram_bank0_1_we;
 assign data_ram_bank1_1_addr = data_ram_bank0_1_addr;
 assign data_ram_bank1_1_wdata = data_ram_bank0_1_wdata;
-/*name*/(
+data_ram_bank data_ram_bank1_1(
 .clka(clk),
 .wea(data_ram_bank1_1_we),
 .addra(data_ram_bank1_1_addr),
@@ -450,14 +486,10 @@ assign data_ram_bank1_1_wdata = data_ram_bank0_1_wdata;
 .douta(data_ram_bank1_1_rdata));
 
 //data_ram_bank2_1
-wire [3:0] data_ram_bank2_1_we;
-wire [7:0] data_ram_bank2_1_addr;
-wire [31:0]data_ram_bank2_1_wdata;
-wire [31:0]data_ram_bank2_1_rdata;
 assign data_ram_bank2_1_we =  data_ram_bank0_1_we;
 assign data_ram_bank2_1_addr = data_ram_bank0_1_addr;
 assign data_ram_bank2_1_wdata = data_ram_bank0_1_wdata;
-/*name*/(
+data_ram_bank data_ram_bank2_1(
 .clka(clk),
 .wea(data_ram_bank2_1_we),
 .addra(data_ram_bank2_1_addr),
@@ -465,14 +497,10 @@ assign data_ram_bank2_1_wdata = data_ram_bank0_1_wdata;
 .douta(data_ram_bank2_1_rdata));
 
 //data_ram_bank3_1
-wire [3:0] data_ram_bank3_1_we;
-wire [7:0] data_ram_bank3_1_addr;
-wire [31:0]data_ram_bank3_1_wdata;
-wire [31:0]data_ram_bank3_1_rdata;
 assign data_ram_bank3_1_we =  data_ram_bank0_1_we;
 assign data_ram_bank3_1_addr = data_ram_bank0_1_addr;
 assign data_ram_bank3_1_wdata = data_ram_bank0_1_wdata;
-/*name*/(
+data_ram_bank data_ram_bank3_1(
 .clka(clk),
 .wea(data_ram_bank3_1_we),
 .addra(data_ram_bank3_1_addr),
@@ -496,7 +524,7 @@ module dirty_ram(
 
 reg [255:0] rf;
 always @(posedge clk) begin
-    if(!resetn)
+    if (!resetn)
         rf <= 0;
     else if (we)
         rf[waddr]<= wdata;
